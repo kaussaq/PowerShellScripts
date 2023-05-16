d4137398d4856d6e7bc587a3d44ec2b1ffc7de8f
kaussaq <tombuckfield@gmail.com>
2023-05-16 10:49:02 +0100

    Changed script to include access rights

--- Bulk_Update_SharedMailbox.ps1
+++ Bulk_Update_SharedMailbox.ps1
@@ -1,13 +1,14 @@
 Connect-ExchangeOnline 
 
 #Provide Shared Mailbox you want to bulk-update
-$SharedMailbox = Read-Host "Please Enter Shared Mailbox address"
+$SharedMailbox = Read-Host "Please enter Shared Mailbox address:"
+$AccessRights = Read-Host "Please enter Access rights:"
 
 #Import CSV of UPN's from C:\temp\users.csv and begin for-loop adding each to the distribution list above
 Import-CSV 'C:\temp\users.csv' | ForEach-Object {  
  $UPN=$_.UPN 
- Write-Progress -Activity "Adding $UPN to group… " 
- Add-DistributionGroupMember –Identity $SharedMailbox -Member $UPN  
+ Write-Progress -Activity "Adding $UPN to Mailbox… " 
+ Add-MailboxPermission –Identity $SharedMailbox -Member $UPN -AccessRights $AccessRights
  If($?)  
  {  
  Write-Host $UPN Successfully added -ForegroundColor Green 
